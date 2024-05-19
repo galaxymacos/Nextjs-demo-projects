b2c0f0f9abb415068a69c41c51ff42f5932ea1cd
Xun Ruan <xunruan@icloud.com>
2024-05-19 23:34:38 +0800

    use hono and tanstack for type safety in client

--- finance-tutorial-with-antonio/src/db/drizzle.ts
+++ finance-tutorial-with-antonio/src/db/drizzle.ts
@@ -1,5 +1,8 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import { accounts } from "@/db/schema";
 
 export const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql);
+
+const account2 = db.select().from(accounts);
